f6be8a3646d672eb7bf3b37e8f5325f12f61d86a
Matthew Schoultz <matthew.schoultz@gmail.com>
2023-08-27 20:35:43 +0200

    Working square Root and Powers Powers not working after 2^3

--- app/src/main/java/com/varsitycollege/st10297294/MainActivity.kt
+++ app/src/main/java/com/varsitycollege/st10297294/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +16,6 @@ class MainActivity : AppCompatActivity() {
         /*Variable declarations*/
         var num1 = findViewById<EditText>(R.id.num1)
         var num2 = findViewById<EditText>(R.id.num2)
-        var result: Int = 0
         val answerView = findViewById<TextView>(R.id.answerView)
         val addButton = findViewById<Button>(R.id.addButton)
         val subtractButton = findViewById<Button>(R.id.subtractButton)
@@ -25,33 +26,56 @@ class MainActivity : AppCompatActivity() {
 
         /*Add Button code*/
         addButton.setOnClickListener {
-            var addNum1 = num1.text.toString().toInt()
-            var addNum2 = num2.text.toString().toInt()
+            var addNum1 = num1.text.toString().toDouble()
+            var addNum2 = num2.text.toString().toDouble()
             var result = addNum1 + addNum2
             answerView.text = "$addNum1 + $addNum2 = $result"
         }
 
         /*subtract Button code*/
         subtractButton.setOnClickListener {
-            var addNum1 = num1.text.toString().toInt()
-            var addNum2 = num2.text.toString().toInt()
+            var addNum1 = num1.text.toString().toDouble()
+            var addNum2 = num2.text.toString().toDouble()
             var result = addNum1 - addNum2
             answerView.text = "$addNum1 - $addNum2 = $result"
         }
 
         /*multiply Button code*/
         multiplyButton.setOnClickListener {
-            var addNum1 = num1.text.toString().toInt()
-            var addNum2 = num2.text.toString().toInt()
+            var addNum1 = num1.text.toString().toDouble()
+            var addNum2 = num2.text.toString().toDouble()
             var result = addNum1 * addNum2
             answerView.text = "$addNum1 X $addNum2 = $result"
         }
         /*Divide Button code*/
         divButton.setOnClickListener {
-            var addNum1 = num1.text.toString().toInt()
-            var addNum2 = num2.text.toString().toInt()
+            var addNum1 = num1.text.toString().toDouble()
+            var addNum2 = num2.text.toString().toDouble()
             var result = addNum1 / addNum2
+            if (addNum2 == 0.0){
+                answerView.text = "Cannot Divide by 0"
+                Toast.makeText(this,"Error!",Toast.LENGTH_SHORT).show()
+            } else
             answerView.text = "$addNum1 ÷ $addNum2 = $result"
+        }
+        /*SquareRoot Button code*/
+        sqButton.setOnClickListener {
+            var addNum1 = num1.text.toString().toDouble()
+            var result = sqrt(addNum1).toInt()
+            answerView.text = "Sqrt($addNum1)= $result X $result"
+        }
+        /*Powers Button code*/
+        pwButton.setOnClickListener {
+            var addNum1 = num1.text.toString().toDouble()
+            var addNum2 = num2.text.toString().toInt()
+            var pwrStore: Double = 0.0
+            var result: Double = 0.0
+            for(pwrStore in 0 until addNum2)
+            {
+                result=addNum1*(pwrStore*addNum1)
+            }
+
+            answerView.text = "$addNum1^$addNum2 = $result"
         }
     }
 }
