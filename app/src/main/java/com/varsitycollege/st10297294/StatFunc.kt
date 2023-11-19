fe449e48829c819df53e4c31f202eacbf3032e61
Matthew Schoultz <matthew.schoultz@gmail.com>
2023-11-19 12:00:30 +0200

    Layout for StatFunc

--- app/src/main/java/com/varsitycollege/st10297294/StatFunc.kt
+++ app/src/main/java/com/varsitycollege/st10297294/StatFunc.kt
@@ -1,14 +1,27 @@
 package com.varsitycollege.st10297294
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Window
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class StatFunc : AppCompatActivity() {
-    private lateinit var text1: TextView
+    lateinit var InputNum: EditText
+    lateinit var SumofTotalArray: TextView
+    lateinit var SumofTotalNum: TextView
+    var arr:ArrayList<Int> = ArrayList(0)
+
+    @SuppressLint("MissingInflatedID")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_stat_func)
-        var text1 = findViewById<TextView>(R.id.text1)
+        SumofTotalArray
+        InputNum
+        SumTotalNum
+        val
     }
 }
